4ccb4a51f6bd97f04ed36ceb245c454db185c408
liushuchun <liuscgood@gmail.com>
2016-08-18 19:47:16 +0800

    init

--- README.md
+++ README.md
@@ -1,1 +1,43 @@
 # fetchfileforblog
+该小工具是用来方便我随时保存网上图片到我的七牛空间的
+============
+#使用方法
+## 一、先修改:
+1. accKey和secKey 为你自己的key
+2. domain也是你自己bucket绑定的域名
+3. 你自己bucket的名字
+
+## 二、编译
+可以直接clone下来，之后
+```
+go build fetch.go
+也可以直接 go get 下来
+再go install
+```
+
+## 三、添加路径
+把当前路径 curpath加入你的.zshrc,活着bash_profile中
+```
+如果是build到当前目录，则需要这一步，
+添加到.zshrc
+export PATH=$PATH:curpath
+```
+
+# 用法
+```
+fetch -url=http://images.cnblogs.com/cnblogs_com/yjf512/201206/201206142215457646.png
+```
+
+output:
+
+```
+the target_list is [http:  images.cnblogs.com cnblogs_com yjf512 201206 201206142215457646.png]fetching http://images.cnblogs.com/cnblogs_com/yjf512/201206/201206142215457646.png
+fetch success
+the response url:
+
+http://oa8dpdexh.bkt.clouddn.com//test2016-08-18 19:22:39.416431907 +0800 CST
+
+```
+
+返回的路径可以供你写入markdown博客。
+
---
+++ fetch.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"strings"
+	"time"
+
+	"qiniupkg.com/api.v7/conf"
+	"qiniupkg.com/api.v7/kodo"
+)
+
+var (
+	//指定需要抓取到的空间
+	bucket = "test"
+	//指定需要抓取的文件的url，必须是公网上面可以访问到的
+	//指定抓取保存到空间的文件的key指
+	key    = "test" + time.Now().String()
+	domain = "http://oa8dpdexh.bkt.clouddn.com/"
+	accKey = "acckeyxxx"
+	secKey = "secxxx"
+)
+
+func main() {
+	targetUrl := flag.String("url", "www.qiniu.com", "输入图片地址")
+
+	flag.Parse()
+
+	lTarget := strings.Split(*targetUrl, "/")
+	key = lTarget[len(lTarget)-1]
+	conf.ACCESS_KEY = accKey
+	conf.SECRET_KEY = secKey
+	fmt.Printf("fetching %s\n", *targetUrl)
+	//new一个Bucket对象
+	c := kodo.New(0, nil)
+	p := c.Bucket(bucket)
+
+	//调用Fetch方法
+
+	err := p.Fetch(nil, key, *targetUrl)
+	if err != nil {
+		fmt.Println("bucket.Fetch failed:", err)
+	} else {
+		fmt.Println("fetch success\nthe response url:\n")
+		fmt.Println(domain + "/" + key)
+	}
+
+}
